264d36e68fc79db34a3b35d43ae5bc6bf90f3e01
mostafa <mostafaahmadi462@gmail.com>
2020-08-04 13:41:56 +0430

    add Photo Entity

--- src/main/java/com/example/demo/model/Photo.java
+++ src/main/java/com/example/demo/model/Photo.java
@@ -1,4 +1,31 @@
 package com.example.demo.model;
 
+import javax.persistence.*;
+import java.sql.Blob;
+import java.util.Date;
+
+@Entity
 public class Photo {
+
+    @Id
+    @Column(length = 5)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE , generator = "sequence" )
+    @SequenceGenerator( name = "sequence" , initialValue = 1 , allocationSize = 1)
+    private int id;
+
+    @Column(length = 120)
+    private String Title;
+
+    @Column(length = 255)
+    private String description;
+
+    @Column(length = 20)
+    private String privacy;
+
+    @Column
+    private Date uploadDate;
+
+    @Column
+    @Lob
+    private Blob View;
 }
