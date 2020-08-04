5147f541fb0c441b17b5b7132c0b5964b5b7c2bb
mostafa <mostafaahmadi462@gmail.com>
2020-08-04 13:51:56 +0430

    add PhotoRepository

--- src/main/java/com/example/demo/JpaRepository/PhotoRepository.java
+++ src/main/java/com/example/demo/JpaRepository/PhotoRepository.java
@@ -1,4 +1,7 @@
 package com.example.demo.JpaRepository;
+import com.example.demo.model.Photo;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PhotoRepository {
+public interface PhotoRepository extends JpaRepository<Photo , Integer> {
+
 }
