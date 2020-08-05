6cf8b205234c63cd39bfa5e551be025998294624
4mocpmo <4mocpmo@gmail.com>
2020-08-05 11:35:33 +0430

    Test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,23 +1,33 @@
 package com.example.demo;
 
 import com.example.demo.jpaRepository.PhotoRepository;
-import com.example.demo.model.Photo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @SpringBootTest
 class DemoApplicationTests {
+	@Autowired
+	PhotoRepository jpaRepository;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void test(){
+//		for (int i = 0 ; i < 4622 ; i ++){
+//			Photo photo = new Photo();
+//			photo.setUploadDate(new Date());
+//			photo.setTitle(RandomString.make().toString());
+//			photo.setPrivacy(RandomString.make().toUpperCase().toString());
+//			photo.setDescription(RandomString.make().toUpperCase().toString());
+//			photo.setView(new Random().nextInt());
+//			jpaRepository.save(photo);
+//		}
+//		List<Photo> photoList = jpaRepository.findAll();
+//		photoList.forEach(System.out::println);
+		jpaRepository.findByPrivacy("home").forEach(System.out::println);
 	}
 }
