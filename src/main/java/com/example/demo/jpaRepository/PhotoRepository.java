554437641ddea6017c6cbe866ccd61f6beb51f7f
mostafa <mostafaahmadi462@gmail.com>
2020-08-05 10:38:21 +0430

    add findBYPrivacy method

--- src/main/java/com/example/demo/jpaRepository/PhotoRepository.java
+++ src/main/java/com/example/demo/jpaRepository/PhotoRepository.java
@@ -1,8 +1,8 @@
 package com.example.demo.jpaRepository;
 import com.example.demo.model.Photo;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
 
 public interface PhotoRepository extends JpaRepository<Photo , Integer> {
 
+    public Photo findByPrivacy(String privacy);
 }
