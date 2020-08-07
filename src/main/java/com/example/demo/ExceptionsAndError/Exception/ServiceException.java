1ba7bf0a18cb8bc1251b7ba0aca0b240a3647078
4mocpmo <4mocpmo@gmail.com>
2020-08-08 03:22:11 +0430

    create exception

--- src/main/java/com/example/demo/ExceptionsAndError/Exception/ServiceException.java
+++ src/main/java/com/example/demo/ExceptionsAndError/Exception/ServiceException.java
@@ -1,4 +1,24 @@
 package com.example.demo.ExceptionsAndError.Exception;
 
-public class ServiceException {
+import java.util.function.Supplier;
+
+public class ServiceException extends Exception{
+    public ServiceException() {
+    }
+
+    public ServiceException(String message) {
+        super(message);
+    }
+
+    public ServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ServiceException(Throwable cause) {
+        super(cause);
+    }
+
+    public ServiceException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
