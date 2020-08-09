38740ba1c5fb8d85f2b817ea191c97b3f2d7b317
4mocpmo <4mocpmo@gmail.com>
2020-08-09 09:27:32 +0430

    add one exception

---
+++ src/main/java/com/example/demo/ExceptionsAndError/Exception/EntityNotExistsException.java
@@ -0,0 +1,22 @@
+package com.example.demo.ExceptionsAndError.Exception;
+
+public class EntityNotExistsException extends Exception {
+    public EntityNotExistsException() {
+    }
+
+    public EntityNotExistsException(String message) {
+        super(message);
+    }
+
+    public EntityNotExistsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public EntityNotExistsException(Throwable cause) {
+        super(cause);
+    }
+
+    public EntityNotExistsException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
